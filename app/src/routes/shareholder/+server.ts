0b3d6b381e425c811a025e7e5352876fb5de2371
Mateusz Zając <matzayonc@gmail.com>
2024-02-17 23:18:38 +0100

    renamed endpoints

---
+++ app/src/routes/shareholder/+server.ts
@@ -0,0 +1,52 @@
+import { anchorStore } from '$stores/anchorStore';
+import { get } from 'svelte/store';
+
+export async function GET({ url }) {
+	const owner = url.searchParams.get('owner');
+	if (!owner) {
+		return new Response('Missing owner', { status: 400 });
+	}
+
+	const { program } = get(anchorStore);
+
+	const stakeholders = await program.account.shareholder.all([
+		{
+			memcmp: {
+				offset: 8,
+				bytes: owner
+			}
+		}
+	]);
+
+	const issuers = await program.account.issuer.all();
+
+	const parsed = JSON.stringify(
+		stakeholders.map((s) => {
+			const issuer = issuers.find((i) => i.publicKey.equals(s.account.issuer));
+			if (!issuer) {
+				return new Response('Issuer not found', { status: 404 });
+			}
+
+			const emissionDate = new Date(s.account.timestamp.toNumber());
+			const registrationDate = new Date(issuer.account.timestamp.toNumber());
+
+			return {
+				issuer_name: issuer.account.name,
+				registration_date: registrationDate.toISOString(),
+				owner_name: s.account.name,
+				paper_number_from: s.account.first.toNumber(),
+				paper_number_to: s.account.amount.add(s.account.first).toNumber(),
+				isin: issuer.account.usingIsin,
+				paper_isin_number: s.account.amount.toNumber(),
+				emission_date: emissionDate.toISOString(),
+				nominal_value: s.account.amount.muln(issuer.account.value).toNumber()
+			};
+		})
+	);
+
+	return new Response(parsed, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
