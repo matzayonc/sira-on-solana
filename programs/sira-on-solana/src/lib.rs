c011d9620cf825886489bf78258d96cdf255c80a
Mateusz Zając <matzayonc@gmail.com>
2024-02-17 08:56:56 +0100

    init issuer

--- programs/sira-on-solana/src/lib.rs
+++ programs/sira-on-solana/src/lib.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 use anchor_lang::prelude::*;
 
 declare_id!("4kKhxNRFxnxXeYn1kfjkkzWpUW91rcuERgaV8qobhk3M");
@@ -6,10 +8,30 @@ declare_id!("4kKhxNRFxnxXeYn1kfjkkzWpUW91rcuERgaV8qobhk3M");
 pub mod sira_on_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name: String, krs: String) -> Result<()> {
+        let issuer = &mut ctx.accounts.issuer;
+        issuer.new(name, krs);
         Ok(())
     }
 }
 
+#[account]
+pub struct Issuer {
+    pub name: String,
+    pub krs: String,
+}
+
+impl Issuer {
+    pub fn new(&mut self, name: String, krs: String) {
+        *self = Self { name, krs };
+    }
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + size_of::<Issuer>())]
+    pub issuer: Account<'info, Issuer>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
--- tests/sira-on-solana.ts
+++ tests/sira-on-solana.ts
@@ -1,16 +1,46 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { SiraOnSolana } from "../target/types/sira_on_solana";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { SiraOnSolana } from "../target/types/sira_on_solana"
+import { Keypair, PublicKey } from "@solana/web3.js"
+import { assert } from "chai"
+
+// @ts-ignore
+const signer = anchor.getProvider().wallet.publicKey
 
 describe("sira-on-solana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	// Configure the client to use the local cluster.
+	anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.SiraOnSolana as Program<SiraOnSolana>;
+	const program = anchor.workspace.SiraOnSolana as Program<SiraOnSolana>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+	it("Is initialized!", async () => {
+		const provider = anchor.getProvider()
+
+		const issuer = Keypair.generate()
+
+		const name = "DotWave"
+		const krs = "1234"
+
+		// const [issuer, issuerBump] = await PublicKey.findProgramAddress(
+		// 	[anchor.utils.bytes.utf8.encode("issuer"), signer.toBuffer()],
+		// 	program.programId
+		// )
+
+		// Add your test here.
+		const tx = await program.methods
+			.initialize(name, krs)
+			.accounts({
+				signer,
+				issuer: issuer.publicKey,
+			})
+			.signers([issuer])
+			.rpc()
+
+		const issuerAccount = await program.account.issuer.fetch(
+			issuer.publicKey
+		)
+
+		assert.equal(issuerAccount.name, name)
+		assert.equal(issuerAccount.krs, krs)
+	})
+})
